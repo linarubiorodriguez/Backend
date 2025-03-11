16a45a9850f20bb658e45728d9010fa69b15ba77
“Lina” <linarubio28987@gmail.com>
2025-03-11 11:06:28 -0500

    Cambios en las vistas de categorias

---
+++ flaskr/__init__.py
@@ -0,0 +1,112 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .modelos import db
+# __init__.py de flaskr
+from .modelos import db, Usuario, Rol, Producto, Categoria, Factura, DetalleFactura, Proveedor, Carrito, DetalleCarrito
+from werkzeug.security import generate_password_hash
+
+def create_app(config_name):
+    app = Flask(__name__)
+    USER_DB = 'root'
+    PASS_DB = ''
+    URL_DB = 'localhost'
+    NAME_DB = 'bdelesconditeanimal'
+    FULL_URL_DB = f'mysql+pymysql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'tu_clave_secreta_segura'
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    with app.app_context():
+        db.create_all()
+        insertar_datos_iniciales() 
+        crear_superadmin()
+
+    return app
+
+
+def insertar_datos_iniciales():
+
+    from .modelos import TipoDoc, Rol, Categoria, MetodoPago, db
+
+    tipos_documento = [
+        {"id": 1, "nombre": "TI", "descripcion": "Tarjeta de identidad"},
+        {"id": 2, "nombre": "CC", "descripcion": "Cédula de ciudadanía"},
+        {"id": 3, "nombre": "CE", "descripcion": "Cédula de extranjería"},
+        {"id": 4, "nombre": "TE", "descripcion": "Tarjeta de extranjería"}
+    ]
+    for tipo in tipos_documento:
+        if not TipoDoc.query.get(tipo["id"]):
+            nuevo_tipo = TipoDoc(id_TipoDocumento=tipo["id"], Nombre=tipo["nombre"], Descripcion=tipo["descripcion"])
+            db.session.add(nuevo_tipo)
+
+    roles = [
+        {"id": 1, "nombre": "Administrador", "descripcion": "Administrador del sistema"},
+        {"id": 2, "nombre": "Cliente", "descripcion": "Usuario del sistema"},
+        {"id": 3, "nombre": "Empleado", "descripcion": "Registrado en el sistema"}
+    ]
+    for rol in roles:
+        if not Rol.query.get(rol["id"]):
+            nuevo_rol = Rol(id_Rol=rol["id"], Nombre=rol["nombre"], Descripcion=rol["descripcion"])
+            db.session.add(nuevo_rol)
+
+    categorias = [
+        {"id": 1, "nombre": "Camas", "descripcion": "Comodidad para tus mascotas.", "imagen": "https://res.cloudinary.com/dvzzqjlbj/image/upload/v1740162149/camas_w31iw0.png"},
+        {"id": 2, "nombre": "Juguetes", "descripcion": "Entretenimiento para tus mascotas.", "imagen": "https://res.cloudinary.com/dvzzqjlbj/image/upload/v1740156258/xfamhkvn2kzklrxbcbgf.png"},
+        {"id": 3, "nombre": "Accesorios", "descripcion": "Estética para tus mascotas.", "imagen": "https://res.cloudinary.com/dvzzqjlbj/image/upload/v1740156259/fiuz63szmeexuvlm6s6u.png"},
+        {"id": 4, "nombre": "Comida", "descripcion": "Alimento para tus mascotas.", "imagen": "https://res.cloudinary.com/dvzzqjlbj/image/upload/v1740156258/ixxmvuvctk6jtx5qj1gv.png"}
+    ]
+    
+    for cat in categorias:
+            categoria_existente = Categoria.query.filter_by(id_categoria=cat["id"]).first()
+            if not categoria_existente:
+                nueva_categoria = Categoria(
+                    id_categoria=cat["id"],
+                    nombre=cat["nombre"],
+                    descripcion=cat["descripcion"],
+                    imagen=cat["imagen"] 
+                )
+                db.session.add(nueva_categoria)
+
+    db.session.commit()
+
+    metodos_pago = [
+        {"id": 1, "nombre": "Paypal"},
+        {"id": 2, "nombre": "VISA"},
+        {"id": 3, "nombre": "Mastercard"},
+        {"id": 4, "nombre": "Nequi"}
+    ]
+
+    for metodo in metodos_pago:
+        if not MetodoPago.query.get(metodo["id"]):
+            nuevo_metodo = MetodoPago(id_pago=metodo["id"], nombre=metodo["nombre"])
+            db.session.add(nuevo_metodo)
+
+
+    db.session.commit()
+
+
+def crear_superadmin():
+    from .modelos import Usuario, db
+    superadmin = Usuario.query.filter_by(id_usuario=1).first()
+    if not superadmin:
+        nuevo_admin = Usuario(
+            id_usuario=1,
+            nombres='Paola',
+            apellidos='Sanchez',
+            telefono='3133334',
+            email='paola01@example.com',
+            num_documento='1111111111',
+            tipo_doc=2,
+            direccion='Calle 123',
+            estado='Activo',
+            id_rol=1
+        )
+        nuevo_admin.contrasena = 'El1234Escondite5656Animal42224235'  # Usar el setter
+        db.session.add(nuevo_admin)
+        db.session.commit()
+
---
+++ flaskr/modelos/modelos.py
@@ -0,0 +1,193 @@
+from marshmallow import fields
+from flask_sqlalchemy import SQLAlchemy
+import enum
+from werkzeug.security import generate_password_hash, check_password_hash
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class TipoDoc(db.Model):
+    __tablename__ = 'tipo_doc'
+    id_TipoDocumento = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(50), nullable=False)
+    Descripcion = db.Column(db.String(255))
+
+class Rol(db.Model):
+    __tablename__ = 'rol'
+    id_Rol = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(50), nullable=False)
+    Descripcion = db.Column(db.String(255))
+
+class MetodoPago(db.Model):
+    __tablename__ = 'metodo_pago'
+    id_pago = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+class Categoria(db.Model):
+    __tablename__ = 'categoria'
+    id_categoria = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(255))
+    imagen = db.Column(db.String(255))  # Nuevo campo para la URL de la imagen
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String(50), nullable=False)
+    apellidos = db.Column(db.String(50), nullable=False)
+    telefono = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    num_documento = db.Column(db.String(50), unique=True, nullable=False)
+    tipo_doc = db.Column(db.Integer, db.ForeignKey('tipo_doc.id_TipoDocumento'), nullable=False)
+    direccion = db.Column(db.String(50))
+    contrasena_hash = db.Column(db.String(128))
+    estado = db.Column(db.String(50), default="Activo", nullable=False)
+    id_rol = db.Column(db.Integer, db.ForeignKey('rol.id_Rol'), nullable=False)
+
+    tipo_documento = db.relationship('TipoDoc', backref='usuarios')
+    rol = db.relationship('Rol', backref='usuarios')
+
+    @property
+    def contrasena(self):
+        raise AttributeError("La contraseña no es un atributo legible.")
+    
+    @contrasena.setter
+    
+    def contrasena(self, contrasena):
+        self.contrasena_hash = generate_password_hash(contrasena, method='pbkdf2:sha256')
+
+    def verificar_contrasena(self, contrasena):
+        resultado = check_password_hash(self.contrasena_hash, contrasena)
+        return resultado
+
+    def tiene_rol(self, rol_nombre):
+        return self.rol and self.rol.Nombre == rol_nombre
+    
+    def esta_activo(self):
+        return self.estado == "Activo"
+
+# Clase Proveedor
+class Proveedor(db.Model):
+    id_proveedor = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    telefono = db.Column(db.String(50))
+    correo = db.Column(db.String(50))
+    estado = db.Column(db.String(50))
+
+# Clase Producto
+class Producto(db.Model):
+    __tablename__ = 'producto'
+    id_producto = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    descripcion = db.Column(db.String(255))
+    precio = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    imagen = db.Column(db.String(255))
+    estado = db.Column(db.String(50), default="Disponible")
+
+    id_categoria = db.Column(db.Integer, db.ForeignKey('categoria.id_categoria'), nullable=False)
+    categoria = db.relationship('Categoria', backref='productos')
+
+class Factura(db.Model):
+    __tablename__ = 'factura'
+    id_factura = db.Column(db.Integer, primary_key=True)
+    fecha_factura = db.Column(db.DateTime, nullable=False)
+    total = db.Column(db.Float, nullable=False)
+    iva_total = db.Column(db.Float, nullable=False)
+    estado = db.Column(db.String(50), default="Pendiente")
+    fecha_vencimiento = db.Column(db.DateTime, nullable=False)
+
+    id_cliente = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), nullable=False)
+    cliente = db.relationship('Usuario', backref='facturas')
+
+    id_metodo_pago = db.Column(db.Integer, db.ForeignKey('metodo_pago.id_pago'), nullable=False)
+    metodo_pago = db.relationship('MetodoPago', backref='facturas')
+
+# Modelo DetalleFactura
+class DetalleFactura(db.Model):
+    id_detalle = db.Column(db.Integer, primary_key=True)
+    cantidad = db.Column(db.Integer, nullable=False)
+    subtotal = db.Column(db.Float, nullable=False)
+
+    id_factura = db.Column(db.Integer, db.ForeignKey('factura.id_factura'))
+    factura = db.relationship('Factura', backref='detalles')
+
+    id_producto = db.Column(db.Integer, db.ForeignKey('producto.id_producto'))
+    producto = db.relationship('Producto', backref='detalles')
+
+
+
+class Carrito(db.Model):
+    id_carrito = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'), nullable=False)
+    productos = db.relationship('DetalleCarrito', backref='carrito', lazy=True)
+
+class DetalleCarrito(db.Model):
+    id_detalle = db.Column(db.Integer, primary_key=True)
+    id_carrito = db.Column(db.Integer, db.ForeignKey('carrito.id_carrito'), nullable=False)
+    id_producto = db.Column(db.Integer, db.ForeignKey('producto.id_producto'), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
+
+
+class EnumField(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        return value.name if value else None
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        return self.enum[value]
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
+        load_instance = True
+
+class TipoDocSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
+        load_instance = True
+
+class RolSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
+        load_instance = True
+
+class MetodoPagoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = MetodoPago
+        include_relationships = True
+        load_instance = True
+
+class CategoriaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Categoria
+        include_relationships = True
+        load_instance = True
+
+class ProveedorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Proveedor
+        include_relationship = True
+        load_instance = True
+
+
+class ProductoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Producto
+        include_relationship = True
+        load_instance = True
+
+class FacturaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Factura
+        include_relationship = True
+        load_instance = True
+
+class DetalleFacturaSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = DetalleFactura
+        include_relationship = True
+        load_instance = True
